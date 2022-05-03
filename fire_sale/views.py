265584c1e599f0dd081c34930eb68a272b75a78f
Birna Rún Karlsdóttir <birnarun@Birnas-MacBook-Pro.local>
2022-05-03 14:10:23 +0000

    first commit

---
+++ fire_sale/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home_page(request):
+    return render(request, 'fire_sale/navigation_bar.html')
+
+
+def about(request):
+    return render(request, 'fire_sale/about.html', {
+        'list': [
+            {
+                'name': 'Arnar',
+                'description': 'Desc 1'
+            },
+            {
+                'name': 'Arnar2',
+                'description': 'Desc 2'
+            }
+        ]
+    })
+
+
+#def index(request):
+#    return HttpResponse('HttpResponse: Hello World')
