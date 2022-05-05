f69740cb33e3992fe0a94a509ba073ac00888fb3
helenasagusts <helenaa19@ru.is>
2022-05-05 11:56:12 +0000

    migrations baett vid og met tag i head

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# user_profile table name
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.CharField(max_length=9999)
+    bio = models.CharField(max_length=9999)
