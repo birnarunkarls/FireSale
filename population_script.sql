83c7238a390add29a8e0f1a85c24d70609f0fca9
Birna Rún Karlsdóttir <birnarun@Birnas-MacBook-Pro.local>
2022-05-10 12:15:00 +0000

    breytingar

--- population_script.sql
+++ population_script.sql
@@ -22,9 +22,14 @@ INSERT INTO checkout_shipping (method, full_name, street_name, house_number, pos
 
 INSERT INTO item_images (item_id, image) VALUES (2, 'https://images.hindustantimes.com/tech/img/2021/12/21/960x540/iPhone_13_Mini_(9)_1632560822480_1640057457018.jpg')
 INSERT INTO item_images (item_id, image) VALUES (2, 'https://images.hindustantimes.com/tech/img/2022/03/16/960x540/iPhone_13_Mini_(10)_1633111870000_1647407172205.jpg')
-
-
-
-
+INSERT INTO item_images (item_id, image) VALUES (3, 'https://vintage-vendor.com/wp-content/gallery/vintage-racing-bikes/vintage-racing-bike-9.jpg')
+INSERT INTO item_images (item_id, image) VALUES (4, 'https://149426355.v2.pressablecdn.com/wp-content/uploads/2020/05/13pro-keyboard.jpg')
+INSERT INTO item_images (item_id, image) VALUES (4, 'https://i.insider.com/5eb3ff15fc593d5396502623?width=750&format=jpeg&auto=webp')
+INSERT INTO item_images (item_id, image) VALUES (4, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44_0sh9xG8hwhjNGfyvLdvAlbYZR-0T1DfS8xtKeqe2nDO715wiTHtHruyIcdxZdEnDA&usqp=CAU')
+INSERT INTO item_images (item_id, image) VALUES (5, 'https://m.media-amazon.com/images/I/41GrPyMMjlL.jpg')
+INSERT INTO item_images (item_id, image) VALUES (5, 'https://www.marcotec-shop.com/pub/media/catalog/product/cache/815b290a519d918302a1a2945471f3a8/s/e/sennheiser_xsw2-835_1.jpg')
+INSERT INTO item_images (item_id, image) VALUES (5, 'https://assets.fatllama.com/images/medium/sennheiser-g3-handheld-wireless-microphone--receiver-21050828.jpg')
+INSERT INTO item_images (item_id, image) VALUES (6, 'https://di2ponv0v5otw.cloudfront.net/posts/2022/04/02/624894c2691412e8b215c421/s_624894f7463d4fa9c0a3ef6e.jpg')
+INSERT INTO item_images (item_id, image) VALUES (6, 'https://i.etsystatic.com/12131993/r/il/c2ffe7/3574192585/il_340x270.3574192585_7iie.jpg')
 
 
